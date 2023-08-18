6fb29de6553d300ad38c823569939cdd2b47bb0a
user.email <jobayer.ahmed109@gmail.com>
2023-08-18 14:00:02 +0600

    function simple experiment

--- function.js
+++ function.js
@@ -6,9 +6,19 @@ function myFunction(a, b) {
 
 // console.log(x);
 
+// function toCelsius(fahrenheit) {
+//   return (5 / 9) * (fahrenheit - 32);
+// }
+
+// let value = toCelsius(58);
+// console.log(typeof value.toFixed(2));
+
 function toCelsius(fahrenheit) {
   return (5 / 9) * (fahrenheit - 32);
 }
 
-let value = toCelsius(58);
-console.log(typeof value.toFixed(2));
+let value1 = toCelsius(58);
+let value2 = toCelsius;
+console.log(value2);
+console.log(value1);
+// console.log(typeof value.toFixed(2));
