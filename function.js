f7872846293eaf02794ac44c04edfc39c95a4b0a
user.email <jobayer.ahmed109@gmail.com>
2023-08-18 13:14:10 +0600

    function peremeter test

--- function.js
+++ function.js
@@ -4,4 +4,11 @@ function myFunction(a, b) {
   return a + b;
 }
 
-console.log(x);
+// console.log(x);
+
+function toCelsius(fahrenheit) {
+  return (5 / 9) * (fahrenheit - 32);
+}
+
+let value = toCelsius(58);
+console.log(typeof value.toFixed(2));
