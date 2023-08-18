b2cce0852710108b49c57a0d52af2c18932f7145
user.email <jobayer.ahmed109@gmail.com>
2023-08-19 00:48:24 +0600

    ............................

---
+++ strict.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var x = 4;
+var x = 5;
+
+// console.log(x);
+
+//
+x = 5;
+// console.log(x)
+
+// ("use strict");
+myFunction();
+
+function myFunction() {
+  y = 3.14; // This will also cause an error because y is not declared
+  //   console.log(y);
+}
