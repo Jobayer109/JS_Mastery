1205ef5e8df1aeb1e54adb41d1fbd5dc2128132a
user.email <jobayer.ahmed109@gmail.com>
2023-08-20 14:04:26 +0600

    String explored

---
+++ string.js
@@ -0,0 +1,40 @@
+let y = new String("John");
+// console.log(y);
+//
+//
+//
+//
+//
+let a = "John";
+let b = new String("John");
+
+if (a == b) {
+  //   console.log("They are same for ==");
+} else {
+  //   console.log("They are not same for ==");
+}
+
+if (a === b) {
+  //   console.log("They are same for ===");
+} else {
+  //   console.log("They are not same for ===");
+}
+
+//
+//
+//
+//
+//
+let p = "John";
+let q = new String("John");
+// console.log(typeof p);
+// console.log(typeof q);
+
+//
+//
+//
+//
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = text.length;
+// console.log(typeof text);
+// console.log(typeof length);
